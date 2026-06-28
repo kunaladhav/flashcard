a0403b2b0d9fb1be982f513440d26c3f300818f3
Kunal Adhav <kunal1603.adhav@gmail.com>
2026-06-28 12:49:51 +0530

    Fix React Router refresh issue on Vercel

--- src/Pages/FlashcardDetails.jsx
+++ src/Pages/FlashcardDetails.jsx
@@ -40,7 +40,7 @@ const FlashcardDetails = () => {
 
       <p className="text-gray-600 mt-2 leading-7">{selectedDeck.description}</p>
 
-      <div className="grid gird-cols-1 lg:grid-cols-12 gap-6 mt-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 mt-8">
         {/* Left Sidebar */}
         <div className="lg:col-span-3 bg-white rounded-lg shadow-sm p-4 self-start">
           <h3 className="text-sm text-gray-500 mb-4">Flashcards</h3>
